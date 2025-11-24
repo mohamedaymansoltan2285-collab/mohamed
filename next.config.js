6ecb84212ba24f36543bf5a710a11984caf83f53
mohamedaymansoltan2285-collab <mohamedaymansoltan2285@gmail.com>
2025-11-24 22:08:50 +0200

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // أو أي إعدادات موجودة بالفعل
+  // مفيش turbo أو appDir هنا
+};
+
+module.exports = nextConfig;
